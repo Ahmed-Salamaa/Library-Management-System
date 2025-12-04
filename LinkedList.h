fd38ec1c8daf58f7eb8e6b0124b4a4dbccc142d3
roaa jouda <roaajouda16@gmail.com>
2025-12-05 01:00:00 +0200

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,38 +1,123 @@
 #ifndef FILE_LINKED_LIST
 #define FILE_LINKED_LIST
 
-template < typename t >
+#include <string>
+using std::string;
+
+template <typename t>
 class LinkedList
 {
-    private :
+private:
+    struct node
+    {
+        t data;
+        node *next;
 
-        struct node
+        node() : data(), next(nullptr) {}
+        node(t val) : data(val), next(nullptr) {}
+
+        bool operator==(const node &rhs) { return data == rhs.data; }
+        bool operator!=(const node &rhs) { return data != rhs.data; }
+    };
+
+    node *head;
+    int size;
+
+public:
+    LinkedList() : head(nullptr), size(0) {}
+
+    void insert(t obj)
+    {
+        node *newNode = new node(obj);
+
+        if (head == nullptr)
         {
-            t data ;
-            node * next ;
+            head = newNode;
+        }
+        else
+        {
+            node *curr = head;
+            while (curr->next != nullptr)
+                curr = curr->next;
 
-            node () : data () , next ( nullptr ) {} ;
-            node ( t val ) : data ( val ) , next ( nullptr ) {} ;
+            curr->next = newNode;
+        }
 
-            bool operator == ( const node & rhs ) { return data == rhs.data ; }
-            bool operator != ( const node & rhs ) { return data != rhs.data ; }
-        };
+        size++;
+    }
 
-        node * head ;
-        int size ;
+    t search(int id)
+    {
+        node *curr = head;
 
-    public :
+        if (curr == nullptr)
+            throw "There is no data";
 
-        LinkedList () : head ( nullptr ) , size ( 0 ) {} ;
+        while (curr != nullptr)
+        {
+            if (curr->data.getId() == id)
+                return curr->data;
 
-        void insert ( t obj ) ;
-        t search ( int id ) ;
-        t search ( string name ) ;
+            curr = curr->next;
+        }
 
-        void erase ( t obj ) ;
-        void erase ( int id ) ;
+        throw "The data does not exist";
+    }
 
+    t search(string name)
+    {
+        node *curr = head;
 
-} ;
+        if (curr == nullptr)
+            throw "There is no data";
+
+        while (curr != nullptr)
+        {
+            if (curr->data.getTitle() == name)
+                return curr->data;
+
+            curr = curr->next;
+        }
+
+        throw "The data does not exist";
+    }
+
+    void erase(t obj)
+    {
+        if (size == 0)
+            return;
+
+        node *curr = head;
+        if (head->data == obj)
+        {
+            head = head->next;
+            delete curr;
+            size--;
+            return;
+        }
+        node *prev = nullptr;
+        curr = head;
+
+        while (curr != nullptr)
+        {
+            if (curr->data == obj)
+            {
+                prev->next = curr->next;
+                delete curr;
+                size--;
+                return;
+            }
+
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+
+    void erase(int id)
+    {
+        t obj = search(id); 
+        erase(obj);
+    }
+};
 
 #endif
